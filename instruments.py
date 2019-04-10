2072cdf7cdf5b7fe58590b51ef9bd78c4e667194
egli <stephan.egli@psi.ch>
2019-04-10 18:53:37 +0200

    Added data file and proper HDF decoding needed for JSON transformation

--- instruments.py
+++ instruments.py
@@ -13,6 +13,29 @@
 import h5py
 import numpy as np
 
+def decodeHDF(va):
+    # turn byte strings to normal strings
+    if isinstance(va, bytes):
+        try:
+            resultValue = va.decode("utf-8")
+            pass
+        except Exception as e:
+            resultValue = va.decode("latin1")
+        return resultValue
+    elif isinstance(va, np.ndarray):
+        # convert byte arrays to string array
+        newArray = []
+        for a in va:
+            if isinstance(a, bytes):
+                newArray.append(a.decode("utf-8"))
+            else:
+                newArray.append(a)
+        return newArray
+    elif isinstance(va, np.float) and math.isnan(va):
+        return None
+    else:
+        return va.item()
+
 def readMetaData(filename):
     if filename.find('sans') >= 0:
         return readSANS(filename)
@@ -40,57 +63,59 @@ def readSANS(filename):
     f = h5py.File(filename,'r')
     meta = {}
     entry = f['entry1']
-    meta['title'] = str(entry['title'][0])
-    meta['collection_description'] = str(entry['comment'][0])
-    meta['start_time'] = str(entry['start_time'][0])
-    meta['end_time'] = str(entry['end_time'][0])
-    meta['instrument'] = np.string_('SANS')
+    meta['title'] = decodeHDF(entry['title'][0])
+    meta['collection_description'] = decodeHDF(entry['comment'][0])
+    # todo normalize times to RFC format
+    meta['start_time'] = decodeHDF(entry['start_time'][0])
+     # todo normalize times to RFC format
+    meta['end_time'] = decodeHDF(entry['end_time'][0])
+    meta['instrument'] = 'SANS'
     source = {}
-    source['type'] = np.string_('Spallation Neutron Source')
-    source['probe'] = np.string_('neutron')
-    source['name'] = np.string_('SINQ at PSI')
+    source['type'] = 'Spallation Neutron Source'
+    source['probe'] = 'neutron'
+    source['name'] = 'SINQ at PSI'
     meta['source'] = source
-    meta['wavelength'] = entry['SANS/Dornier-VS/lambda'][0]
+    meta['wavelength'] = decodeHDF(entry['SANS/Dornier-VS/lambda'][0])
     coll = {}
-    coll['shape'] = np.string_('nxcylinder')
-    coll['size'] = entry['SANS/collimator/length'][0]
+    coll['shape'] = 'nxcylinder'
+    coll['size'] = decodeHDF(entry['SANS/collimator/length'][0])
     meta['collimator'] = coll
     detector = {}
-    detector['sum'] = np.sum(entry['SANS/detector/counts'])
-    detector['distance'] = entry['SANS/detector/x_position'][0]
+    # todo find proper transformation detector['sum'] = np.sum(decodeHDF(entry['SANS/detector/counts']))
+    detector['distance'] = decodeHDF(entry['SANS/detector/x_position'][0])
     detector['polar_angle'] = 0.
     detector['azimuthal_angle'] = 0.
     detector['aequatorial_angle'] = 0.
     detector['x_pixel_size'] = 7.5
     detector['y_pixel_size'] = 7.5
     if pathExists(entry,'SANS/detector/beam_center_x'.split('/')):
-        detector['beam_center_x'] = entry['SANS/detector/beam_center_x'][0]*7.5
-        detector['beam_center_y'] = entry['SANS/detector/beam_center_y'][0]*7.5
+        detector['beam_center_x'] = decodeHDF(entry['SANS/detector/beam_center_x'][0])*7.5
+        detector['beam_center_y'] = decodeHDF(entry['SANS/detector/beam_center_y'][0])*7.5
     meta['detector'] = detector
     sample = {}
-    sample['name'] = str(entry['sample/name'][0])
-    sample['environment'] = str(entry['sample/environment'][0])
+    sample['name'] = decodeHDF(entry['sample/name'][0])
+    sample['environment'] = decodeHDF(entry['sample/environment'][0])
     sample['aequatorial_angle'] = 0.
     if pathExists(entry,'sample/temperature'.split('/')):
-        sample['temperature'] = entry['sample/temperature'][0]
+        sample['temperature'] = decodeHDF(entry['sample/temperature'][0])
     else:
-        sample['temperature'] = np.string_('UNKNOWN')
+        sample['temperature'] = 'UNKNOWN'
     if pathExists(entry,'sample/magnet'.split('/')):
-        sample['magnet'] = entry['sample/magnet'][0]
+        sample['magnet'] = decodeHDF(entry['sample/magnet'][0])
     else:
-        sample['magnet'] = np.string_('UNKNOWN')
+        sample['magnet'] = 'UNKNOWN'
     meta['sample'] = sample
     control = {}
-    control['mode'] = entry['SANS/detector/count_mode'][0]
-    control['preset'] = entry['SANS/detector/preset'][0]
-    control['time'] = entry['SANS/detector/counting_time'][0]
-    control['integral'] = entry['SANS/monitor1/counts'][0]
-    control['monitor2'] = entry['SANS/monitor2/counts'][0]
-    control['monitor3'] = entry['SANS/monitor3/counts'][0]
+    control['mode'] = decodeHDF(entry['SANS/detector/count_mode'][0])
+    control['preset'] = decodeHDF(entry['SANS/detector/preset'][0])
+    control['time'] = decodeHDF(entry['SANS/detector/counting_time'][0])
+    control['integral'] = decodeHDF(entry['SANS/monitor1/counts'][0])
+    control['monitor2'] = decodeHDF(entry['SANS/monitor2/counts'][0])
+    control['monitor3'] = decodeHDF(entry['SANS/monitor3/counts'][0])
     meta['control'] = control
-    meta['user'] = entry['user/name'][0]
-    meta['experiment_identifier'] = str(entry['proposal_id'][0])
-    meta['attenuator'] = entry['SANS/attenuator/selection'][0]
+    meta['user'] = decodeHDF(entry['user/name'][0])
+    meta['experiment_identifier'] = decodeHDF(entry['proposal_id'][0])
+    # todo: commented because not existing: meta['attenuator'] = decodeHDF(entry['SANS/attenuator/selection'][0])
     f.close()
     return meta
 
