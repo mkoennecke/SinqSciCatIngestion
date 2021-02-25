237b90b1643bc0a9031bd98da602740e03b9f83b
Philip Hertel <philiphertel@localhost.localdomain>
2021-02-25 10:56:55 +0100

    added focusingest.py

--- sansingest.py
+++ sansingest.py
@@ -82,53 +82,53 @@ def readSANS(filename):
 
 def writeDataset(numor, fname,  scientificmeta, token):
 
-        filenameList='intermediate/filelisting-'+str(numor)+'.txt'
-        filelist = open(filenameList,'w') 
-        filelist.write(fname)
-        filelist.close()
- 
-        # print(scientificmeta)
-        proposalId='20.500.11935/'+scientificmeta['experiment_identifier'].replace(' ','')
-        print(proposalId)
+    filenameList='intermediate/filelisting-'+str(numor)+'.txt'
+    filelist = open(filenameList,'w') 
+    filelist.write(fname)
+    filelist.close()
 
-        url='https://dacat-qa.psi.ch/api/v3/Proposals/'+urllib.parse.quote_plus(proposalId)+'?access_token='+token
-        r = requests.get(url)
-        if(r.status_code != 200):
-            print('Proposal Error Result:',url,r.text)
-        else:
-            proposal= json.loads(r.text)
+    # print(scientificmeta)
+    proposalId='20.500.11935/'+scientificmeta['experiment_identifier'].replace(' ','')
+    print(proposalId)
 
-        # create metadata infos from data in proposal and scientific meta data
+    url='https://dacat-qa.psi.ch/api/v3/Proposals/'+urllib.parse.quote_plus(proposalId)+'?access_token='+token
+    r = requests.get(url)
+    if(r.status_code != 200):
+        print('Proposal Error Result:',url,r.text)
+    else:
+        proposal= json.loads(r.text)
 
-            meta = {}
-            meta['principalInvestigator']=proposal['pi_email']
-            meta['creationLocation'] = proposal['MeasurementPeriodList'][0]['instrument']
-            meta['dataFormat'] = 'SANS-NEXUS-HDF5'
-            meta['sourceFolder'] = root
-            meta['owner']=proposal['firstname']+proposal['lastname']
-            meta['ownerEmail']=proposal['email']
-            meta['type']='raw'
-            # TODO decide what fields to add to description
-            meta['description']=scientificmeta['title']+" / collection:"+scientificmeta['collection_description']
-            temp='undefined'
-            if 'temperature' in scientificmeta['sample']:
-                tempCandidate=scientificmeta['sample']['temperature']
-                if isinstance(tempCandidate, float):
-                    temp='%.1f' % tempCandidate
-               
-            meta['datasetName']=scientificmeta['user']+"-"+scientificmeta['sample']['name']+"-T="+temp
-            meta['ownerGroup']=proposal['ownerGroup']
-            meta['accessGroups']=proposal['accessGroups']
-            meta['proposalId']=proposalId
-            meta['scientificMetadata']=scientificmeta
-            # create metadata.json file
-            filenameMeta='intermediate/metadata-'+str(year)+'-'+str(start)+'.json'
-            metafile = open(filenameMeta,'w') 
-            metafile.write(json.dumps(meta, indent=3, sort_keys=True))
-            metafile.close()
-            # run datasetIngestor command
-            subprocess.call(["./datasetIngestor","-testenv", "-ingest", "-allowexistingsource", "-token", token, filenameMeta, filenameList])
-             # todo remove files in "intermediate" folder
+    # create metadata infos from data in proposal and scientific meta data
+
+        meta = {}
+        meta['principalInvestigator']=proposal['pi_email']
+        meta['creationLocation'] = proposal['MeasurementPeriodList'][0]['instrument']
+        meta['dataFormat'] = 'SANS-NEXUS-HDF5'
+        meta['sourceFolder'] = root
+        meta['owner']=proposal['firstname']+proposal['lastname']
+        meta['ownerEmail']=proposal['email']
+        meta['type']='raw'
+        # TODO decide what fields to add to description
+        meta['description']=scientificmeta['title']+" / collection:"+scientificmeta['collection_description']
+        temp='undefined'
+        if 'temperature' in scientificmeta['sample']:
+            tempCandidate=scientificmeta['sample']['temperature']
+            if isinstance(tempCandidate, float):
+                temp='%.1f' % tempCandidate
+            
+        meta['datasetName']=scientificmeta['user']+"-"+scientificmeta['sample']['name']+"-T="+temp
+        meta['ownerGroup']=proposal['ownerGroup']
+        meta['accessGroups']=proposal['accessGroups']
+        meta['proposalId']=proposalId
+        meta['scientificMetadata']=scientificmeta
+        # create metadata.json file
+        filenameMeta='intermediate/metadata-'+str(year)+'-'+str(start)+'.json'
+        metafile = open(filenameMeta,'w') 
+        metafile.write(json.dumps(meta, indent=3, sort_keys=True))
+        metafile.close()
+        # run datasetIngestor command
+        subprocess.call(["./datasetIngestor","-testenv", "-ingest", "-allowexistingsource", "-token", token, filenameMeta, filenameList])
+            # todo remove files in "intermediate" folder
 
 
 # ======================== main loop ===========================
